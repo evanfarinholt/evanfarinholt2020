611af72a4aaf3abdb8379514117dd6c9bb7aaad7
Evan Farinholt <evanfarinholt@evans-mbp-2.home>
2020-08-18 17:02:40 -0400

    testing out router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import Home from "./Pages/Home";
 
 class App extends Component {
     render(){
         return (
             <BrowserRouter>
+            <ul>
+                <li><Link to={'/'}>Home</Link></li>
+            </ul>
                 <Switch>
                     <Route exact path={'/'} render={() => <Home />} />
                 </Switch>
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 class Home extends Component {
     render(){
         return (
-            <div>
-                Home
-            </div>
+            <Container fluid={true}>
+                <Row>
+                    <Col>Home</Col>
+                </Row>
+            </Container>
         )
     }
 }
