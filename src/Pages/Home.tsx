a626121f432137b347b5c706a49b53c7b8cbe6f4
Evan Farinholt <evanfarinholt@evans-mbp-2.home>
2020-08-18 22:59:07 -0400

    landing page set up and ready to push

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -6,9 +6,9 @@ class Home extends Component {
         return (
             <Container fluid={true}>
                 <Row>
-                    <Col className="d-flex justify-content-center flex-column">
-                        <h1 className="text-center">Evan Farinholt</h1>
-                        <p className="text-center">Website Coming Soon</p>
+                    <Col style={{"height": "100vh"}} className="d-flex justify-content-center flex-column">
+                        <h1 style={{"color": "#3DABAE"}} className="text-center">Evan Farinholt</h1>
+                        <h2 style={{"color": "#3DABAE"}} className="text-center">Website Coming Soon</h2>
                         <p className="text-center">efarinholt@gmail.com</p>
                         <p className="text-center">(201) 470-0402</p>
                     </Col>
