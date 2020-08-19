92d3176118370f1b8a998fae46c69f63f880cd13
Evan Farinholt <evanfarinholt@evans-mbp-2.home>
2020-08-18 22:46:56 -0400

    setting up temp landing page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -6,8 +6,11 @@ class Home extends Component {
         return (
             <Container fluid={true}>
                 <Row>
-                    <Col>
-                        <h1>Home</h1>
+                    <Col className="d-flex justify-content-center flex-column">
+                        <h1 className="text-center">Evan Farinholt</h1>
+                        <p className="text-center">Website Coming Soon</p>
+                        <p className="text-center">efarinholt@gmail.com</p>
+                        <p className="text-center">(201) 470-0402</p>
                     </Col>
                 </Row>
             </Container>
