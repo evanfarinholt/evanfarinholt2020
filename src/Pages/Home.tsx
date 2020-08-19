3ffdefd16d136dcb35f7128c77ce1071e936a4c4
Evan Farinholt <evanfarinholt@evans-mbp-2.home>
2020-08-19 00:01:21 -0400

    adjusting styles

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { StyleHelper } from "../Helpers/StyleHelper";
 
 class Home extends Component {
     render(){
@@ -7,10 +8,10 @@ class Home extends Component {
             <Container fluid={true}>
                 <Row>
                     <Col style={{"height": "100vh"}} className="d-flex justify-content-center flex-column">
-                        <h1 style={{"color": "#3DABAE"}} className="text-center">Evan Farinholt</h1>
-                        <h2 style={{"color": "#3DABAE"}} className="text-center">Website Coming Soon</h2>
-                        <p className="text-center">efarinholt@gmail.com</p>
-                        <p className="text-center">(201) 470-0402</p>
+                        <h1 style={StyleHelper.header()} className="text-center">Evan Farinholt</h1>
+                        <h2 style={StyleHelper.headerSecondary()} className="text-center">Website Coming Soon</h2>
+                        <p style={StyleHelper.paragraph()} className="text-center">efarinholt@gmail.com</p>
+                        <p style={StyleHelper.paragraph()} className="text-center">(201) 470-0402</p>
                     </Col>
                 </Row>
             </Container>
