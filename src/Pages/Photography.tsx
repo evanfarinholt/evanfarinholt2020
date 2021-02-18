8dd2d8cf67c32984ccae24751156ae8dd9be8766
Evan Farinholt <evanfarinholt@evans-mbp-2.home>
2021-02-18 16:49:24 -0500

    building lightbox using bootstrap carousel

--- src/Pages/Photography.tsx
+++ src/Pages/Photography.tsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Carousel } from 'react-bootstrap';
 import { Colors } from "../Helpers/Enums";
 //import { Text, TextType } from "../Components/Text";
 import styled from "styled-components";
 import Gallery from "react-photo-gallery";
 
-export default class Photography extends Component {
+
+interface IProps {}
+interface IState {
+    isLightboxOpen: boolean;
+    currentImage: number;
+}
+export default class Photography extends Component<IProps, IState>  {
+    constructor(props: IProps){
+        super(props);
+        this.state = {
+            isLightboxOpen: false,
+            currentImage: 0
+        }
+    }
     render(){
         const Section1 = styled.header`
             background-image: linear-gradient(258.8deg, ${Colors.DkGray} 23.68%, ${Colors.MdGray} 106.16%);
@@ -27,37 +40,72 @@ export default class Photography extends Component {
               src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
               width: 4,
               height: 3,
-              name: "testing"
-
+              title: "testing"
             },
             {
               src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
               width: 1,
-              height: 1
-            },
+              height: 1,
+              title: "testing 2"
+            }
         ]
 
 
 
-        const openLightbox = (event: any, photo: any) => {
-            console.log(photo)
-            // setCurrentImage(index);
-            // setViewerIsOpen(true);
+        const handleOpenLightbox = (event: any, photo: any) => {
+            //console.log(photo)
+            this.setState({
+                isLightboxOpen: true,
+                currentImage: photo.index
+            });
         }; 
-
+        const handleCloseLightbox = () => {
+            this.setState({
+                isLightboxOpen: false
+            });
+        }
+        const handleCarouselSelect = (index: number) => {
+            this.setState({
+                currentImage: index
+            });
+        }
         return (
             <>
-            <Container>
-                <Row>
-                    <Col>
-                        <h1>photo</h1>
-                        <Gallery 
-                            photos={photos} 
-                            onClick={openLightbox} 
-                        />
-                    </Col>
-                </Row>
-            </Container>
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1>photo</h1>
+                            <Gallery 
+                                photos={photos} 
+                                onClick={handleOpenLightbox} 
+                                direction={"column"}
+                            />
+                            {this.state.isLightboxOpen &&
+                                <Carousel
+                                    activeIndex={this.state.currentImage}
+                                    onSelect={handleCarouselSelect}
+                                    indicators={false}
+                                >
+                                    {photos.map((photo: any, i: number) => {
+                                        return(
+                                            <Carousel.Item key={i}>
+                                                <img
+                                                    className="d-block w-100"
+                                                    src={photo.src}
+                                                    alt={photo.title}
+                                                />
+                                                <Carousel.Caption>
+                                                    <p>{photo.title}</p>
+                                                </Carousel.Caption>
+                                            </Carousel.Item>
+                                        )
+                                    })}
+                                </Carousel>
+                            }
+                            
+                        </Col>
+                    </Row>
+                </Container>
             </>
         )
     }
