5c27c54aa2b6c6785ddb7abde469b7522f81efc9
Evan Farinholt <evanfarinholt@evans-mbp-2.home>
2021-07-15 11:10:49 -0400

    more style updates

--- src/Pages/Music.tsx
+++ src/Pages/Music.tsx
@@ -5,7 +5,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
-import styled from "styled-components";
 import { events } from '../Helpers/Enums';
 
 interface IProps {}
@@ -14,25 +13,7 @@ interface IState {
     currentImage: number;
 }
 export default class Photography extends Component<IProps, IState>  {
-    constructor(props: IProps){
-        super(props);
-    }
     render(){
-
-        const EventBlock = styled.div`
-            display: flex;
-            flex-direction: row;
-
-            .date-time-block {
-                width: 20%;
-            }
-            .title-block {
-                width: 50%;
-            }
-            .address-block {
-                width: 30%;
-            }
-        `;
 
         const handleBookingButtonClick = (url: string) => {
             window.open(url, "_blank");
@@ -40,9 +21,9 @@ export default class Photography extends Component<IProps, IState>  {
 
         const socialLink = (icon: JSX.Element, label: string, url: string) => 
             <div className="social-link-container d-flex flex-row">
-                <a href={url} target="_blank" rel="noopener noreferrer" className="d-flex flex-row">
-                    {icon}
-                    {label}
+                <a href={url} target="_blank" rel="noopener noreferrer" className="link-primary d-flex flex-row pr-3">
+                    <div className="pr-2">{icon}</div>
+                    <span className="text-white special-link">{label}</span>
                 </a>
             </div>;
 
@@ -53,51 +34,52 @@ export default class Photography extends Component<IProps, IState>  {
             </button>
         
         const eventItem = (date: string, time: string, title: string, venue: string, address: string, cityState: string) => 
-            <EventBlock>
-                <div className="date-time-block d-flex flex-column">
-                    <span>{date}</span>
+            <div className="d-flex flex-row">
+                <div className="w-25 p-2 date-time-block d-flex flex-column">
+                    <span><strong>{date}</strong></span>
                     <span>{time}</span>
                 </div>
-                <div className="title-block d-flex flex-column">
-                    <span>{title}</span>
+                <div className="w-50 p-2 title-block d-flex flex-column">
+                    <span><strong>{title}</strong></span>
                     <span>{venue}</span>
                 </div>
-                <div className="address-block d-flex flex-column">
+                <div className="w-25 p-2 address-block d-flex flex-column">
                     <span>{address}</span>
                     <span>{cityState}</span>
                 </div>
-            </EventBlock>
+            </div>
 
         return (
             <>
-                <Container className="pt-5 pb-5">
-                    <Row>
-                        <Col> 
+                <Container>
+                    <Row className="pt-5 pb-5">
+                        <Col md={9} className="d-flex flex-column justify-content-center"> 
                             <h1>Evan Farinholt</h1>
                             <h2>Musician &amp; Educator</h2>
+                            <div className="d-flex flex-row pt-2 pb-2">
+                                {socialLink(
+                                    <FontAwesomeIcon className="text-primary" icon={faInstagram} /> 
+                                    , "evanfarinholt"
+                                    , "https://www.instagram.com/evanfarinholt/"
+                                )}
+                                {socialLink(
+                                    <FontAwesomeIcon className="text-primary" icon={faEnvelope} /> 
+                                    , "efarinholt@gmail.com"
+                                    , "mailto:efarinholt@gmail.com"
+                                )}
+                            </div>
                         </Col>
-                    </Row>
-                    <Row>
-                        <Col className="d-flex flex-row">
-                            {socialLink(
-                                <FontAwesomeIcon className="link-icon" icon={faInstagram} /> 
-                                , "evanfarinholt"
-                                , "https://www.instagram.com/evanfarinholt/"
-                            )}
-                            {socialLink(
-                                <FontAwesomeIcon className="link-icon" icon={faEnvelope} /> 
-                                , "efarinholt@gmail.com"
-                                , "mailto:efarinholt@gmail.com"
-                            )}
+                        <Col md={3} className="d-none d-md-flex justify-content-end d-sm-none hidden-xs-down">
+                            <img alt="" className="img-fluid" src="/maine-circle-graphic.svg" />
                         </Col>
                     </Row>
                     <Row className="pt-3 pb-3">
-                        <Col md={12} lg={9}>
+                        <Col md={12} lg={10}>
                             <h3>I’m a <strong>drummer</strong>, <strong>drum teacher</strong>, and <strong>music maker</strong> based out of Kittery Maine.</h3>
                         </Col>
                     </Row>
                     <Row>
-                        <Col md={12} lg={6}>
+                        <Col md={12} lg={8}>
                             <p>I’m available for <strong>teaching</strong>, <strong>performing</strong>, and <strong>song writing projects</strong> in the Seacoast region. Book a session with me if you’re interested.</p>
                         </Col>
                     </Row>
@@ -110,7 +92,7 @@ export default class Photography extends Component<IProps, IState>  {
                     <Row className="pt-3 pb-3">
                         <Col>
                             <h4>Upcoming Events</h4>
-                            <span>Summer/Fall '21</span>
+                            <span className="special-label">Summer/Fall '21</span>
                         </Col>
                     </Row>
                     <Row className="pt-3 pb-3">
--- src/Pages/Photography.tsx
+++ src/Pages/Photography.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Carousel } from 'react-bootstrap';
-import { Colors } from "../Helpers/Enums";
+//import { Colors } from "../Helpers/Enums";
 //import { Text, TextType } from "../Components/Text";
-import styled from "styled-components";
+//import styled from "styled-components";
 import Gallery from "react-photo-gallery";
 
 
@@ -20,20 +20,20 @@ export default class Photography extends Component<IProps, IState>  {
         }
     }
     render(){
-        const Section1 = styled.header`
-            background-image: linear-gradient(258.8deg, ${Colors.DkGray} 23.68%, ${Colors.MdGray} 106.16%);
-            .inner-wrapper {
-                background-image: url('/button-vector.svg');
-                background-repeat: no-repeat;
-                background-position: top right;
-                background-size: 400px;
-            }
-            @media (max-width: 872px) {
-                .inner-wrapper {
-                    background-image: none;
-                }
-            }
-        `;
+        // const Section1 = styled.header`
+        //     background-image: linear-gradient(258.8deg, ${Colors.DkGray} 23.68%, ${Colors.MdGray} 106.16%);
+        //     .inner-wrapper {
+        //         background-image: url('/button-vector.svg');
+        //         background-repeat: no-repeat;
+        //         background-position: top right;
+        //         background-size: 400px;
+        //     }
+        //     @media (max-width: 872px) {
+        //         .inner-wrapper {
+        //             background-image: none;
+        //         }
+        //     }
+        // `;
         
         const photos = [
             {
@@ -59,11 +59,11 @@ export default class Photography extends Component<IProps, IState>  {
                 currentImage: photo.index
             });
         }; 
-        const handleCloseLightbox = () => {
-            this.setState({
-                isLightboxOpen: false
-            });
-        }
+        // const handleCloseLightbox = () => {
+        //     this.setState({
+        //         isLightboxOpen: false
+        //     });
+        // }
         const handleCarouselSelect = (index: number) => {
             this.setState({
                 currentImage: index
